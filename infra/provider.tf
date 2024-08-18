782dbc1970ca44a24bfc04a9d3de92672fcc7899
Markme44cvsu <markjohn.ignacio@cvsu.edu.ph>
2024-08-19 06:40:51 +0800

    Backend - used S3 Bucket as backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,18 +1,23 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = ">= 4.9.0"
-        }
-        http = {
-            source  = "hashicorp/http"
-            version = "~> 2.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 2.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-s3-state-mark-abc123"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    shared_credentials_files = ["C:\\Users\\Mark John\\.aws\\credentials"]
+  region                   = "us-east-1"
+  shared_credentials_files = ["C:\\Users\\Mark John\\.aws\\credentials"]
 
 }
